5853c356c25f3ed5d72467216d5eb20b4a35b837
Björn Marschollek <git@muffix.de>
2022-08-19 16:52:15 +0200

    Add keyboard shortcut to open the page on 12ft.io
    Adds a keyboard shortcut (Shift+Option+P by default) to open the page in
    the current tab on 12ft.io.
    
    The shortcut can be customised by the user in the browser settings.
    
    Resolves #2

--- src/scripts/bg.js
+++ src/scripts/bg.js
@@ -1,4 +1,22 @@
-chrome.browserAction.onClicked.addListener((tab) => {
-    const url = 'https://12ft.io/' +  tab.url;
+function onCommand(command) {
+    if (command === '_cmd_openIn12ft') {
+        // Browser Action popup pages aren't part of any tab, so tabs.getCurrent() won't work.
+        // We need to use browser.tabs.query instead.
+        browser.tabs.query({ currentWindow: true, active: true }, tabs => {
+            if (tabs && tabs[0].url.startsWith('http')) {
+                reopenIn12ft(tabs[0]);
+            }
+        });
+    }
+}
+
+function reopenIn12ft(tab) {
+    const url = `https://12ft.io/${tab.url}`;
     browser.tabs.update(tab.id, {url});
+}
+
+chrome.browserAction.onClicked.addListener((tab) => {
+    reopenIn12ft(tab);
 });
+
+browser.commands.onCommand.addListener(onCommand);
